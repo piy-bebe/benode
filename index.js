7d66b458fcf8d8579dc0bfa936c2a4e93df300af
bebe <piy.bebe@gmail.com>
2021-02-25 22:35:44 +0300

    test

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+app.listen(PORT, () => {
+  console.log(`Server has been started on port ${PORT}...`)
+})
