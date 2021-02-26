b5e82bf61ee052604a42fa1440bdffe30b6c169c
bebe <piy.bebe@gmail.com>
2021-02-26 14:41:55 +0300

    created navbar

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs',
 })
-
+app.use(express.static('public'))
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
@@ -14,13 +14,23 @@ app.set('views', 'views')
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    title: 'Home',
+    isHome: true,
+  })
 })
-app.get('/about', (req, res) => {
-  res.render('about')
+app.get('/courses', (req, res) => {
+  res.render('courses', {
+    title: 'Courses',
+    isCourses: true,
+  })
+})
+app.get('/add', (req, res) => {
+  res.render('add', {
+    title: 'Add',
+    isAdd: true,
+  })
 })
 app.listen(PORT, () => {
-  console.log(
-    `[!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!vey] Server has been started on port ${PORT}...`
-  )
+  console.log(`Server has been started on port ${PORT}...`)
 })
