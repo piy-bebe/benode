581b0796015f2b03a0c6502fa82a0924a4c64d72
piy-bebe <piy.bebe@gmail.com>
2021-02-27 00:37:20 +0300

    Release routes

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const app = express()
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
 
-const routerHome = require('./routes/home')
+const routerCourses = require("./routes/courses");
+const routerHome = require("./routes/home");
+const routerAdd = require("./routes/add");
 
 const hbs = exphbs.create({
-  defaultLayout: 'main',
-  extname: 'hbs',
-})
-app.use(express.static('public'))
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+  defaultLayout: "main",
+  extname: "hbs",
+});
 
-app.use(routerHome)
+app.use(express.static("public"));
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-const PORT = process.env.PORT || 3000
+app.use(routerHome);
+app.use(routerCourses);
+app.use(routerAdd);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server has been started on port ${PORT}...`)
-})
+  console.log(`Server has been started on port ${PORT}...`);
+});
