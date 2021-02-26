abaefe61d97092f8419046e5b07fc6c29b7fccfd
bebe <piy.bebe@gmail.com>
2021-02-27 00:18:05 +0300

    Updated routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 
+const routerHome = require('./routes/home')
+
 const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs',
@@ -11,26 +13,10 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(routerHome)
+
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Home',
-    isHome: true,
-  })
-})
-app.get('/courses', (req, res) => {
-  res.render('courses', {
-    title: 'Courses',
-    isCourses: true,
-  })
-})
-app.get('/add', (req, res) => {
-  res.render('add', {
-    title: 'Add',
-    isAdd: true,
-  })
-})
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}...`)
 })
