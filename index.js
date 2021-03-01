4d0afa4164aff3908aad7ab1d67ea1c304d3a7bd
bebe <piy.bebe@gmail.com>
2021-03-01 12:39:08 +0300

    Update route cart

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
-const app = express();
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
 
-const routerCourses = require("./routes/courses");
-const routerHome = require("./routes/home");
-const routerAdd = require("./routes/add");
+const routerCourses = require('./routes/courses')
+const routerHome = require('./routes/home')
+const routerAdd = require('./routes/add')
+const routerCard = require('./routes/card')
 
 const hbs = exphbs.create({
-  defaultLayout: "main",
-  extname: "hbs",
-});
+  defaultLayout: 'main',
+  extname: 'hbs',
+})
 
-app.use(express.static("public"));
-app.use(express.urlencoded({extended: false}))
-app.engine("hbs", hbs.engine);
-app.set("view engine", "hbs");
-app.set("views", "views");
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
-app.use("/", routerHome);
-app.use("/courses", routerCourses);
-app.use("/add", routerAdd);
-
-const PORT = process.env.PORT || 3000;
+app.use('/', routerHome)
+app.use('/courses', routerCourses)
+app.use('/add', routerAdd)
+app.use('/card', routerCard)
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server has been started on port ${PORT}...`);
-});
+  console.log(`Server has been started on port ${PORT}...`)
+})
--- routes/card.js
+++ routes/card.js
@@ -1,8 +1,20 @@
 const { Router } = require('express')
 const router = Router()
-const Course = require('../models/course')
-const Card = require('../models/card')
+// const Course = require('../models/course')
+// const Card = require('../models/card')
 
-router.post('/card/add', async (req, res) => {
-  const course = await Course.getById(req.body.id)
+router.post('/add', async (req, res) => {
+  //   const course = await Course.getById(req.body.id)
+  //   await Card.add(course)
+  res.redirect('/card')
 })
+
+router.get('/', (req, res) => {
+  // const card = await Card.fetch()
+  res.render('card', {
+    title: 'Корзина',
+    //  card
+  })
+})
+
+module.exports = router
