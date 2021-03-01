db7de98241aa902281bca5af1ad9685cf23fc902
piy-bebe <piy.bebe@gmail.com>
2021-03-01 23:45:42 +0300

    Fix bag with route

--- routes/card.js
+++ routes/card.js
@@ -1,19 +1,19 @@
 const { Router } = require('express')
 const router = Router()
-// const Course = require('../models/course')
-// const Card = require('../models/card')
+const Course = require('../models/course')
+const Card = require('../models/card')
 
 router.post('/add', async (req, res) => {
-  //   const course = await Course.getById(req.body.id)
-  //   await Card.add(course)
+    const course = await Course.getById(req.body.id)
+    await Card.add(course)
   res.redirect('/card')
 })
 
-router.get('/', (req, res) => {
-  // const card = await Card.fetch()
+router.get('/', async (req, res) => {
+  const card = await Card.fetch()
   res.render('card', {
     title: 'Корзина2',
-    //  card
+     card
   })
 })
 
