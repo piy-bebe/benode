7c72fe4b2e301de81af05846654e5fc8b0b48c63
piy-bebe <piy.bebe@gmail.com>
2021-03-02 00:24:00 +0300

    card

--- routes/card.js
+++ routes/card.js
@@ -1,20 +1,22 @@
-const { Router } = require('express')
-const router = Router()
-const Course = require('../models/course')
-const Card = require('../models/card')
+const { Router } = require("express");
+const router = Router();
+const Course = require("../models/course");
+const Card = require("../models/card");
 
-router.post('/add', async (req, res) => {
-    const course = await Course.getById(req.body.id)
-    await Card.add(course)
-  res.redirect('/card')
-})
+router.post("/add", async (req, res) => {
+  const course = await Course.getById(req.body.id);
+  await Card.add(course);
+  res.redirect("/card");
+});
 
-router.get('/', async (req, res) => {
-  const card = await Card.fetch()
-  res.render('card', {
-    title: 'Корзина2',
-     card
-  })
-})
+router.get("/", async (req, res) => {
+  const card = await Card.fetch();
+  res.render("card", {
+    title: "Корзина",
+    isCard: true,
+    courses: card.courses,
+    price: card.price,
+  });
+});
 
-module.exports = router
+module.exports = router;
