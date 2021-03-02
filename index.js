3ca10aaafe8ff250f34eca072e06a9d078478a07
piy-bebe <piy.bebe@gmail.com>
2021-03-02 20:42:32 +0300

    mongodb connect

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const app = express()
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require('mongoose')
+const app = express();
 
-const routerCourses = require('./routes/courses')
-const routerHome = require('./routes/home')
-const routerAdd = require('./routes/add')
-const routerCard = require('./routes/card')
+const routerCourses = require("./routes/courses");
+const routerHome = require("./routes/home");
+const routerAdd = require("./routes/add");
+const routerCard = require("./routes/card");
 
 const hbs = exphbs.create({
-  defaultLayout: 'main',
-  extname: 'hbs',
-})
+  defaultLayout: "main",
+  extname: "hbs",
+});
 
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: false }))
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-app.use('/', routerHome)
-app.use('/courses', routerCourses)
-app.use('/add', routerAdd)
-app.use('/card', routerCard)
-const PORT = process.env.PORT || 3000
+app.use("/", routerHome);
+app.use("/courses", routerCourses);
+app.use("/add", routerAdd);
+app.use("/card", routerCard);
+const PORT = process.env.PORT || 3000;
+const password = "12345";
 
-app.listen(PORT, () => {
-  console.log(`Server has been started on port ${PORT}...`)
-})
+
+async function start() {
+  try {
+    const url = `mongodb+srv://piybebe:${password}@cluster0.yriz1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+    await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+    app.listen(PORT, () => {
+      console.log(`Server has been started on port ${PORT}...`);
+    });
+  } catch(e) {
+    console.log(e)
+  }
+}
+start()
