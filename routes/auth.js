8df8990d3440bb246299c58dc877cadeba993dd3
bebe <piy.bebe@gmail.com>
2021-03-15 11:58:05 +0300

    modified auth page

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,10 @@ const { Router } = require('express')
 const router = Router()
 
 router.get('/login', async (req, res) => {
-  res.render('auth/login')
+  res.render('auth/login', {
+    title: 'Sign in',
+    isLogin: true,
+  })
 })
 
 module.exports = router
